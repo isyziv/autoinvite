e0d76a57060bda2c09c3118628f8ac4e29bb381e
Mastersam <abadasamuelosp@gmail.com>
2020-04-29 04:43:58 -0700

    map urls

--- autoinvite/settings.py
+++ autoinvite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'autoinvite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'invite/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
