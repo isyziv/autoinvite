11a519f4b6ed3594cf56036f0bf14101694ca0f4
Mastersam <abadasamuelosp@gmail.com>
2020-04-30 17:33:48 -0700

    fixes

--- autoinvite/settings.py
+++ autoinvite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='')
+SECRET_KEY = 'j1hs0ow0a=z5ahjfig3kg4z2065xv$!ty9m%izc5gegbwlc)n1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
