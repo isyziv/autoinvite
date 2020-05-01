2dca38752b1b535977ba12e528de9ae2fb59ee3c
Mastersam <abadasamuelosp@gmail.com>
2020-05-01 03:09:05 -0700

    fixed error in css

--- autoinvite/settings.py
+++ autoinvite/settings.py
@@ -26,6 +26,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['APPLICATION_NAME']]
 
+
+# ALLOWED_HOSTS = []
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -113,7 +116,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, './invite/static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+import dj_database_url  # some comments
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 APPEND_SLASH = False
 
